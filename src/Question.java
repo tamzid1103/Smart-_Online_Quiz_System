f694e51592078b51547a624c8fb18d8d91cc8768
Tamzidul Haque <tamjidul2003@gmail.com>
2025-07-28 04:47:21 +0600

    Question Handling

---
+++ src/Question.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Question {
+    private String text;
+    private String[] options;
+    private int correctIndex;
+
+    public Question(String text, String[] options, int correctIndex) {
+        this.text = text;
+        this.options = options;
+        this.correctIndex = correctIndex;
+    }
+
+    public void display() {
+        System.out.println(text);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i]);
+        }
+    }
+
+    public boolean checkAnswer(int answer) {
+        return answer == correctIndex;
+    }
+}
